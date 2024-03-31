d10f74752cdedefa2662f9f1b24634787cfb323b
irfash <irfash.dev@gmail.com>
2024-03-31 21:57:00 +0530

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "onedarkpro_onedark"
+config.cursor_blink_rate = 0
+config.enable_tab_bar = false -- Using tmux instead...yes I know!!
+config.font = wezterm.font("Operator Mono", { weight = "Book" })
+config.font_rules = {
+  {
+    intensity = "Bold",
+    italic = false,
+    font = wezterm.font("Operator Mono", { weight = "Medium" }),
+  },
+}
+config.font_size = 13
+-- config.front_end = "WebGpu"
+config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+  { key = "\r", mods = "CTRL", action = wezterm.action({ SendString = "\x1b[13;5u" }) },
+  { key = "\r", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b[13;2u" }) },
+  { key = "LeftArrow", mods = "ALT", action = wezterm.action({ SendString = "\x1b\x62" }) },
+  { key = "RightArrow", mods = "ALT", action = wezterm.action({ SendString = "\x1b\x66" }) },
+}
+config.line_height = 1
+config.mouse_bindings = {
+  -- Open a link
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = "OpenLinkAtMouseCursor",
+  },
+}
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 5,
+  right = 5,
+  top = 0,
+  bottom = 0,
+}
+config.window_background_opacity =0.65
+
+return config
